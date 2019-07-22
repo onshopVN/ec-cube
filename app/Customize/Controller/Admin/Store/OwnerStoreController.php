4253d25db995a1d2bfebd2702dbf8b1aba08f93d
ndquocphong <ndquocphong@gmail.com>
2019-07-22 15:23:26 +0700

    Implement sample html of plugins. Modify eccube extension make eccube understand template & translate from Customize

--- app/Customize/Controller/Admin/Store/OwnerStoreController.php
+++ app/Customize/Controller/Admin/Store/OwnerStoreController.php
@@ -5,7 +5,6 @@ use Knp\Component\Pager\Paginator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpClient\CurlHttpClient;
 use Eccube\Controller\AbstractController;
 
 /**
@@ -18,7 +17,7 @@ class OwnerStoreController extends AbstractController
      *
      * @Route("/search", name="admin_store_plugin_owners_search")
      * @Route("/search/page/{page_no}", name="admin_store_plugin_owners_search_page", requirements={"page_no" = "\d+"})
-     * @Template("@admin/Store/plugin_search.twig")
+     * @Template("@Customize/Admin/Store/OwnerStore/search.twig")
      *
      * @param Request     $request
      * @param int $page_no
@@ -28,12 +27,6 @@ class OwnerStoreController extends AbstractController
      */
     public function search(Request $request, $page_no = null, Paginator $paginator)
     {
-        var_dump($GLOBALS['__composer_autoload_files']);
-        die(__METHOD__);
-        $httpClient = new CurlHttpClient();
-
-//        $response = $httpClient->request('GET', 'http://ownerstore.demo/api/v1/plugins/categories');
-
         return [
 //            'pagination' => $pagination,
 //            'total' => $total,
